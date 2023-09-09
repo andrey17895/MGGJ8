c6cdf33f98d557e5082cdb34d59ccb49a2130112
andrey <a@>
2023-09-09 19:20:22 +0200

    screenshot fixed

--- game/common/screenshot.gd
+++ game/common/screenshot.gd
@@ -1,12 +1,9 @@
 extends Node
 
-var count = 1
-
 func _ready():
     print(OS.get_user_data_dir())
 
 func _input(event):
     if event.is_action_pressed("screenshot"):
         print("screenshot taken")
-        get_viewport().get_texture().get_image().save_png("user://screenshot_" + str(count) + ".png")
-        count += 1
+        get_viewport().get_texture().get_image().save_png("user://screenshot_" + str(Time.get_ticks_msec()) + ".png")

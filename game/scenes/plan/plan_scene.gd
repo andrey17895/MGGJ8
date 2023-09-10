c03a9854542a45f5b876bb2e4638d50cfaebd016
andrey <a@>
2023-09-10 17:05:51 +0200

    plan scene improved

--- game/scenes/plan/plan_scene.gd
+++ game/scenes/plan/plan_scene.gd
@@ -3,21 +3,25 @@ extends Scene
 @export var sprites: Array[Node2D] = []
 @export var comments: Array[Control] = []
 
-var index = 0
+var index = 1
 
 func _ready():
 	assert(sprites.size() == comments.size(), "Unequal number of elements in sprites and comments")
+	_show_picture(0)
+	
+
+func _show_picture(num: int) -> void:
+	for i in range(sprites.size()):
+		sprites[i].visible = false
+		comments[i].visible = false
+	sprites[num].visible = true
+	comments[num].visible = true
+
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("accept_or_click"):
 		if index == sprites.size():
 			_end_scene()
 			return
-		sprites[index].visible = false
-		comments[index].visible = false
+		_show_picture(index)
 		index += 1
-		if index < sprites.size():
-			sprites[index].visible = true
-			comments[index].visible = true
-	pass
-

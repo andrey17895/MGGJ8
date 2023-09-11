52e77328d5dfa26b7d7132d165d653786ef5d66a
andrey <a@>
2023-09-11 21:15:32 +0200

    semi fix for camera bug

--- game/scenes/comix/broken_phone.gd
+++ game/scenes/comix/broken_phone.gd
@@ -1,8 +1,10 @@
 extends Scene
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var camera: Camera2D = $Camera2D
 
 func _ready():
     animation.play("move")
     await animation.animation_finished
+    camera.zoom = Vector2(1, 1)
     _end_scene()
